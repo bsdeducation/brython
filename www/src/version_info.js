ec635b0a1370c43e6c037f078767047cb7cfe797
James Cockshull <jc@bsd.education>
2022-08-03 19:19:23 +0100

    Include updated build artifacts

--- www/src/version_info.js
+++ www/src/version_info.js
@@ -1,8 +1,9 @@
-__BRYTHON__.implementation = [3, 8, 0, 'dev', 0]
+__BRYTHON__.loop_timeout = 3
+__BRYTHON__.implementation = [3, 8, 0, 'bsd_patch', 0]
 __BRYTHON__.__MAGIC__ = "3.8.0"
-__BRYTHON__.version_info = [3, 8, 0, 'final', 0]
-__BRYTHON__.compiled_date = "2019-10-15 12:16:19.491953"
-__BRYTHON__.timestamp = 1571134579491
+__BRYTHON__.version_info = [3, 8, 0, 'bsd_patch', 0]
+__BRYTHON__.compiled_date = "2022-08-03 18:20:59.009894"
+__BRYTHON__.timestamp = 1659547259009
 __BRYTHON__.builtin_module_names = ["_aio",
     "_ajax",
     "_base64",
