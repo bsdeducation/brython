3e62d1873565e549c607f681bf5ecfe97961c7e6
James Cockshull <jc@bsd.education>
2022-08-03 19:37:48 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -euf
+
+BUCKET=bsd-launchbox-learner-dev
+PREFIX=resources/js
+
+# Move the old files out of the way
+aws s3 mv "s3://$BUCKET/$PREFIX/brython_stdlib.3.8.0.js" "s3://$BUCKET/$PREFIX/brython_stdlib.3.8.0.js_old"
+aws s3 mv "s3://$BUCKET/$PREFIX/brython_stdlib.js" "s3://$BUCKET/$PREFIX/brython_stdlib.js_old"
+aws s3 mv "s3://$BUCKET/$PREFIX/brython.3.8.0.js" "s3://$BUCKET/$PREFIX/brython.3.8.0.js_old"
+aws s3 mv "s3://$BUCKET/$PREFIX/brython.js" "s3://$BUCKET/$PREFIX/brython.js_old"
+
+# Upload new files
+aws s3 cp www/src/brython_stdlib.js "s3://$BUCKET/$PREFIX/brython_stdlib.js"
+aws s3 cp www/src/brython_stdlib.js "s3://$BUCKET/$PREFIX/brython_stdlib.3.8.0.js"
+aws s3 cp www/src/brython.js "s3://$BUCKET/$PREFIX/brython.js"
+aws s3 cp www/src/brython.js "s3://$BUCKET/$PREFIX/brython.3.8.0.js"
