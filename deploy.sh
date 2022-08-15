e323dfcb1fe6169d1e4a60ffc0f5e6a5256f7032
James Cockshull <jc@bsd.education>
2022-08-15 12:44:19 +0100

    Rename _old to -nopatch

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ BUCKET=bsd-launchbox-learner-dev
 PREFIX=resources/js
 
 # Move the old files out of the way
-aws s3 mv "s3://$BUCKET/$PREFIX/brython_stdlib.3.8.0.js" "s3://$BUCKET/$PREFIX/brython_stdlib.3.8.0.js_old"
-aws s3 mv "s3://$BUCKET/$PREFIX/brython_stdlib.js" "s3://$BUCKET/$PREFIX/brython_stdlib.js_old"
-aws s3 mv "s3://$BUCKET/$PREFIX/brython.3.8.0.js" "s3://$BUCKET/$PREFIX/brython.3.8.0.js_old"
-aws s3 mv "s3://$BUCKET/$PREFIX/brython.js" "s3://$BUCKET/$PREFIX/brython.js_old"
+aws s3 mv "s3://$BUCKET/$PREFIX/brython_stdlib.3.8.0.js" "s3://$BUCKET/$PREFIX/brython_stdlib.3.8.0-nopatch.js"
+aws s3 mv "s3://$BUCKET/$PREFIX/brython_stdlib.js" "s3://$BUCKET/$PREFIX/brython_stdlib-nopatch.js"
+aws s3 mv "s3://$BUCKET/$PREFIX/brython.3.8.0.js" "s3://$BUCKET/$PREFIX/brython.3.8.0-nopatch.js"
+aws s3 mv "s3://$BUCKET/$PREFIX/brython.js" "s3://$BUCKET/$PREFIX/brython-nopatch.js"
 
 # Upload new files
 aws s3 cp www/src/brython_stdlib.js "s3://$BUCKET/$PREFIX/brython_stdlib.js"
